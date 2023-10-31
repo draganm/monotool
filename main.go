dbf092ae1bfad97a55475ffcd15ce255949031ba
Dragan Milic <dragan@netice9.com>
2023-10-31 15:14:48 +0100

    removed root cli action

--- main.go
+++ main.go
@@ -1,27 +1,17 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/draganm/monotool/command/images"
 	initcommand "github.com/draganm/monotool/command/init"
-	"github.com/draganm/monotool/config"
 	"github.com/urfave/cli/v2"
 )
 
 func main() {
 	app := &cli.App{
+		Name: "monotool",
 		Commands: []*cli.Command{
 			initcommand.Command(),
 			images.Command(),
-		},
-		Action: func(ctx *cli.Context) error {
-			_, err := config.Load()
-			if err != nil {
-				return fmt.Errorf("could not load config: %w", err)
-			}
-
-			return nil
 		},
 	}
 	app.RunAndExitOnError()
